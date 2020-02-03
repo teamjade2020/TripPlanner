ef845396f4b4894878c2262da016f990044e140c
Jeel Naik <jeelsnaik@gmail.com>
2020-02-03 15:05:33 -0800

    made the changes suggested

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :locations
-  resources :trips
+
       devise_for :users
       get '*path', to: 'pages#index', constraints: ->(request){ request.format.html? }
+      resources :locations
+      resources :trips
       root to: 'pages#index'
 end
