42e7dd968adc45a9ee02c1618f182741f3ebe927
ernesto godoy <ernst.godoy@gmail.com>
2020-02-14 15:40:31 -0800

    refactor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,8 +111,8 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
     config.action_mailer.raise_delivery_errors = true
 	config.action_mailer.delivery_method = :smtp
-	host = 'localhost:3000' #replace with your own url
-	config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+	host = 'https://glacial-bastion-31936.herokuapp.com/' #replace with your own url
+	config.action_mailer.default_url_options = { :host => 'https://glacial-bastion-31936.herokuapp.com/', protocol: 'https' }
 
 	# SMTP settings for gmail
 	config.action_mailer.smtp_settings = {
